a994e0034c9d434a9fd4d62273125cd81e30b37c
Petar Petrov <twinsenbg@gmail.com>
2012-03-25 15:32:50 +0300

    Almost complete heading page

--- templates_c/f90be83b235fb03cc225b11607032e9ddd415899.file.index.tpl.php
+++ templates_c/f90be83b235fb03cc225b11607032e9ddd415899.file.index.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.8, created on 2012-03-20 22:24:18
+<?php /* Smarty version Smarty-3.1.8, created on 2012-03-24 22:02:05
          compiled from "templates\index.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:212324f47a73d67a5d4-95978130%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'f90be83b235fb03cc225b11607032e9ddd415899' => 
     array (
       0 => 'templates\\index.tpl',
-      1 => 1332278657,
+      1 => 1332622923,
       2 => 'file',
     ),
   ),
@@ -22,6 +22,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 <?php if ($_valid && !is_callable('content_4f47a73d7b5689_43232986')) {function content_4f47a73d7b5689_43232986($_smarty_tpl) {?><!DOCTYPE HTML>
 <html>
 	<head>
+		<title> GameCraft - Gamification as a Service </title>
 		<link type="text/plain" rel="author" href="http://game-craft.com/humans.txt" />
 		<meta name="keywords" content="GameCraft, Bulgaria, Startup, Gamification as a Service, Gamification, Game Design, Playful Design, Motivational Design" />
 		<meta name="description" content="GameCraft is the first Eastern-European Startup (Based in Bulgaria) that provides Gamification as a Service." />
@@ -33,13 +34,12 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 		<link rel="image_src" href="assets/logo.png" />
 		<link rel="stylesheet" type="text/css" href="styles/main.css" />
 		<link rel="stylesheet" type="text/css" href="styles/reset.css" />
-		<title> GameCraft - Gamification as a Service </title>
 	</head>
 	<body>
 		<div id='nav_wrapper'>
 			<div class='container'>
 				<div id="email">
-					<a class='email' href="mailto:level2@Game-Craft.com"> <img class="email_icon" width='24px' height='14px' src='assets/mail_icon.png' alt="email immage" />level2@Game-Craft.com </a>
+					<a class='email' href="mailto:level2@game-craft.com"> <img class="email_icon" width='24' height='14' src='assets/mail_icon.png' alt="email immage" />level2@game-craft.com </a>
 				</div>
 				<ul id='nav_links'>
 					<li class='nav'>
@@ -72,7 +72,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 		</div>
 		<div id='content_wrapper'>
 			<div id='content'>
-				<div id='left_content'>
+				<div id='top_content'>
 					<div id='who_are_we'>
 						<h2 class='title' title='WHO ARE WE?'>WHO ARE WE?</h2>
 						<img class = 'sunglasses_owl' width='57' height='92' src='assets/sunglasses_owl.png' alt="GameCraft Owl With Sunglasses" />
@@ -91,33 +91,53 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 						by making them more <b>engaging and fun</b>.
 						<br/>
 					</div>
+					<div id="twitter_container">
+						<h2 class='title' title='BigFluffyOwl TWEETS'>BigFluffyOwl TWEETS <img class = 'twitter_logo' width='30' height='21' src='assets/twitter_logo.png' alt="Twitter Logo" /></h2>
+						<ul id="twitter_update_list">
+							<li>
+								Twitter feed loading...
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div id='bottom_content'>
 					<div id='gamification'>
-						<h2 class='title' title='WHAT IS GAMIFICATION?'>WHAT IS GAMIFICATION? </h2>
+						<h2 class='gamification' title='WHAT IS GAMIFICATION?'>WHAT IS GAMIFICATION? </h2>
 						<img class = 'nerdy_owl' width='62' height='88' src='assets/nerdy_owl.png' alt="GameCraft Nerdy Owl" />
 						<div class='bubble'>
 							<div class="bubble-arrow-border"></div>
 							<div class="bubble-arrow"></div>
-							Gamification involves applying <b>game design thinking to non-game applications</b> to make them
+							Gamification involves applying <span>game design thinking to non-game applications</span> to make them
 							more fun and engaging.
 							<br/>
 							Gamification refers to incorporating game elements and mechanics into non-gaming websites and software.
 						</div>
 					</div>
+					<div id="follow_container">
+						<h2 class='title' title='FOLLOW US'>FOLLOW US </h2>
+						<div class="follow_content">
+							Everything around Game-Craft:
+							<br />
+							<a href="https://www.facebook.com/GameCraftBulgaria"><img class = 'follow_logo' width='35' height='35' src='assets/f_facebook.png' alt="Follow us on Facebook" /></a>
+							<a href="https://twitter.com/#!/BigFluffyOwl"><img class = 'follow_logo' width='35' height='35' src='assets/f_twitter.png' alt="Follow us on Twitter" /></a>
+							<a href="http://www.linkedin.com/company/game-craft"><img class = 'follow_logo' width='35' height='35' src='assets/f_linkedin.png' alt="Follow us on LinkedIn" /></a>
+							<a href="http://pinterest.com/gamecraftpin/"><img class = 'follow_logo' width='35' height='35' src='assets/f_pinterest.png' alt="Follow us on Pinterest" /></a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
-		<div id="footer-container">
-			<footer>
-				<div id='footer_img'>
-					<a href="http://www.game-craft.com/" title="GameCraft">Game Craft</a>
-					&nbsp;|&nbsp; <a href="http://www.game-craft.com/blog/about/" title="About GameCraft">About</a>
-					<br/>
-					Copyright 2012 by Game-Craft.
-				</div>
-			</footer>
+		<div id="footer-wrapper">
+			<div id="footer">
+				<a href="http://www.game-craft.com/" title="GameCraft">Game Craft</a>
+				&nbsp;|&nbsp; <a href="http://www.game-craft.com/blog/about/" title="About GameCraft">About</a>
+				<br/>
+				Copyright 2012 by Game-Craft.
+			</div>
 		</div>
 		<?php echo $_smarty_tpl->getSubTemplate ("analytics.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
-
+ <script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
+		<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/BigFluffyOwl.json?callback=twitterCallback2&count=3"></script>
 	</body>
 </html>
 <?php }} ?>
